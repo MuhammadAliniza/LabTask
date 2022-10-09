cf0ef36c63c4c11dbbfb9c0ef744ec3efbb62d86
BuildTools <unconfigured@null.spigotmc.org>
2022-10-09 23:47:33 +0500

    4rd commit

--- LabTask/src/LabTaskOne/Main.java
+++ LabTask/src/LabTaskOne/Main.java
@@ -8,31 +8,38 @@ public class Main {
     public static void main(String[] args) {
 
         //2.	A program which show’s your variable stores even/odd number.
-/*
+
         EvenOdd evenodd = new EvenOdd();
-        evenodd.check();
-*/
+        //evenodd.check();
+
          //4. Create a program in which user is asked for marks in a respected way
          //  (Matriculation , Intermediate and Test result) where output would be the CPN.
-/*
+
         CpnCalculator cpn = new CpnCalculator();
-        cpn.calculator();
-*/
+        //cpn.calculator();
+
         //3.    Create a program using every single operator we have learned till now.
 
         Operators operators = new Operators();
 
         //  Arithmetic Operators
-        operators.arithmetic(12,33);
+
+        //operators.arithmetic(12,33);
+
 
         //  Logical Operators
-        operators.logical(32, 11);
+
+        //operators.logical(32, 11);
+
 
         //  Assignment operators
-        operators.arithmetic(12, 21);
+
+        //operators.arithmetic(12, 21);
+
 
         //  Increment & decrement Operators
-        operators.increment_decrement(12,7);
+
+        //operators.increment_decrement(12,7);
 
     }
 }
